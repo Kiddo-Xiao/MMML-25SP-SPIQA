0f6dbc93ceb0f08fcd123feb44ce97de47ffc48b
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 <yuanxu.derrick@gmail.com>
2025-03-26 22:38:09 -0400

    Failed examples

--- unimodal_3resnet_with_eval.py
+++ unimodal_3resnet_with_eval.py
@@ -3,12 +3,13 @@ import pandas as pd
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from torch.utils.data import DataLoader,Dataset
+from torch.utils.data import DataLoader, Dataset
 from torchvision import transforms, models
 from PIL import Image
 from rouge import Rouge
 import math
 from tqdm import tqdm
+import matplotlib.pyplot as plt
 
 
 # 定义数据集类
@@ -32,7 +33,7 @@ class ImageAnswerDataset(Dataset):
         label = self.answer_to_label[answer]
         if self.transform:
             image = self.transform(image)
-        return image, label
+        return image, label, img_path  # Added img_path to return
 
 
 # 图像预处理
@@ -58,7 +59,7 @@ def train(model, dataloader, criterion, optimizer, device):
     running_loss = 0.0
     correct = 0
     total = 0
-    for images, labels in dataloader:
+    for images, labels, _ in dataloader:  # Updated to unpack img_path
         images, labels = images.to(device), labels.to(device)
         outputs = model(images)
         loss = criterion(outputs, labels)
@@ -72,15 +73,17 @@ def train(model, dataloader, criterion, optimizer, device):
     return running_loss / len(dataloader), 100.0 * correct / total
 
 
-# 评估函数
-def evaluate(model, dataloader, criterion, device):
+# 评估函数和失败案例收集
+def evaluate(model, dataloader, criterion, device, model_name):
     model.eval()
     running_loss = 0.0
     correct = 0
     total = 0
     predictions, references = [], []
+    failed_examples = []  # To store failed examples
+
     with torch.no_grad():
-        for images, labels in tqdm(dataloader, desc="Evaluating", leave=False):
+        for images, labels, img_paths in tqdm(dataloader, desc="Evaluating", leave=False):
             images, labels = images.to(device), labels.to(device)
             outputs = model(images)
             loss = criterion(outputs, labels)
@@ -90,6 +93,35 @@ def evaluate(model, dataloader, criterion, device):
             correct += predicted.eq(labels).sum().item()
             predictions.extend([str(p.item()) for p in predicted])
             references.extend([str(l.item()) for l in labels])
+
+            # Collect failed examples
+            for i in range(len(labels)):
+                if predicted[i] != labels[i]:
+                    failed_examples.append({
+                        'image_path': img_paths[i],
+                        'true_label': labels[i].item(),
+                        'predicted_label': predicted[i].item(),
+                        'true_answer': dataset.label_to_answer[labels[i].item()],
+                        'predicted_answer': dataset.label_to_answer[predicted[i].item()],
+                        'model_name': model_name
+                    })
+
+    # Save failed examples to a CSV file
+    if failed_examples:
+        failed_df = pd.DataFrame(failed_examples)
+        os.makedirs('failed_examples', exist_ok=True)
+        failed_df.to_csv(f'failed_examples/failed_{model_name}.csv', index=False)
+
+        # Save some example images
+        os.makedirs(f'failed_examples/images_{model_name}', exist_ok=True)
+        for i, example in enumerate(failed_examples[:10]):  # Save first 10 examples
+            img = Image.open(example['image_path'])
+            plt.figure()
+            plt.imshow(img)
+            plt.title(f"True: {example['true_answer']}\nPredicted: {example['predicted_answer']}")
+            plt.savefig(f"failed_examples/images_{model_name}/failed_example_{i}.png")
+            plt.close()
+
     rouge = Rouge().get_scores(' '.join(predictions), ' '.join(references))[0]['rouge-l']['f']
     perplexity = math.exp(running_loss / len(dataloader))
     return running_loss / len(dataloader), 100.0 * correct / total, rouge, perplexity
@@ -108,12 +140,14 @@ for resnet_func in resnet_variants:
     optimizer = optim.AdamW(model.parameters(), lr=0.001)
 
     num_epochs = 80
-    print(resnet_func)
+    model_name = resnet_func.__name__
+    print(f"\nTraining {model_name}")
+
     for epoch in tqdm(range(1, num_epochs + 1)):
         train_loss, train_acc = train(model, train_loader, criterion, optimizer, device)
         if epoch % 10 == 0:
             print(f'Epoch {epoch}/{num_epochs}, Train Loss: {train_loss:.4f}, Train Acc: {train_acc:.2f}%')
 
-    val_loss, val_acc, rouge_score, perplexity = evaluate(model, val_loader, criterion, device)
+    val_loss, val_acc, rouge_score, perplexity = evaluate(model, val_loader, criterion, device, model_name)
     print(
-        f'Model: {resnet_func.__name__}, Val Acc: {val_acc:.2f}%, ROUGE: {rouge_score:.4f}, Perplexity: {perplexity:.4f}')
+        f'Model: {model_name}, Val Acc: {val_acc:.2f}%, ROUGE: {rouge_score:.4f}, Perplexity: {perplexity:.4f}')
